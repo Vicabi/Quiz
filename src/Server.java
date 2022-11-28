09a926f4a2c01f0979ac59f49f9c25516af8abba
Vicabi <115193194+Vicabi@users.noreply.github.com>
2022-11-28 12:06:43 +0100

    Added game.start in while loop

--- src/Server.java
+++ src/Server.java
@@ -13,18 +13,7 @@ public class Server {
                 Socket player2 = listener.accept();
                 Game game = new Game(player1, player2);
                 System.out.println("Game skapat");
-                ServerPlayer player1 = new ServerPlayer("Spelare 1",game);
-                System.out.println("Spelare 1 ansluten");
-                ServerPlayer player2 = new ServerPlayer("Spelare 2",game);
-                System.out.println("Spelare 2 ansluten");
-
-                player1.setOpponent(player2);
-                player2.setOpponent(player1);
-                game.player1 = player1;
-                game.player2 = player2;
-                game.currentPlayer = player1;
-                player1.start();
-                player2.start();
+                game.start();
                 System.out.println("Slutet av main loop");
 
             }
